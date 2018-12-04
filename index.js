22af386c27c5403c0da32b2b531dcee4522e77dc
Leonardo Peralta <leo.pm15@gmail.com>
2018-12-04 22:19:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
 var katzDeliLine = []
+var number = 1
 
-      function takeANumber(katzDeliLine, newName ) {
-        katzDeliLine.push(newName);
+      function takeANumber(line) {
 
-        return (`Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`)
+        line.push(number);
+        number =  number + 1
+
+        return (`Welcome`)
       }
 
+
+      someLine = []
+
+      takeANumber(someLine)
+      number ///1
+      someLine // [1]
+      takeANumber(someLine)
+      number //2
+      someLine // [1, 2]
+      takeANumber(someLine)
+      
+      someLine // [1,2, 1]
+      takeANumber(someLine)
+      [1,2,1,2]
+
+    //  takeANumber(katzDeliLine, "Bill")  // ["Bill"]
+      //takeANumber(katzDeliLine, "Kate") // ["Bill", "Kate"]
+      //nowServing(katzDeliLine)
+
+      takeANumber(katzDeliLine) // [1]
+      takeANumber(katzDeliLine) // [1, 2]
+      nowServing(katzDeliLine) // [2]
+      nowServing(katzDeliLine) // []
+
+      takeANumber(katzDeliLine) // [3]
+      takeANumber(katzDeliLine) // [3,4]
+
+
       function nowServing(katzDeliLine) {
-        let i = 0;
 
         if (katzDeliLine.length === 0) {
           return "There is nobody waiting to be served!";
         }
-          else return (`Currently serving ${katzDeliLine.shift()}.`);
+          else {
+            var name = katzDeliLine.shift()
+             return (`Currently serving ${name}.`);
+          }
       }
 
       function currentLine(katzDeliLine) {
